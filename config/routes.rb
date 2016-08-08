be32d0fbe5c2f7a8012919d1518130f03ab0c8b6
Alex <alexduval71@gmail.com>
2016-08-08 23:13:37 +0200

    create controller and views with generate command

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/create'
+
+  get 'tasks/new'
+
+  get 'tasks/edit'
+
+  get 'tasks/show'
+
+  get 'tasks/update'
+
+  get 'tasks/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 end
